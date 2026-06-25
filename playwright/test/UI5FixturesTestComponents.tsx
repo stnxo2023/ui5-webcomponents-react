f382ec5c5c5c8c8561cebf6e4eca6f81669dcd47
Lukas Harbarth <lukas.harbarth@sap.com>
2026-06-25 08:23:59 +0200

    chore: cleanup unused code (#8722)

--- playwright/test/UI5FixturesTestComponents.tsx
+++ playwright/test/UI5FixturesTestComponents.tsx
@@ -1,24 +1,17 @@
 import { Button } from '@ui5/webcomponents-react/Button';
-import { CheckBox } from '@ui5/webcomponents-react/CheckBox';
 import { ComboBox } from '@ui5/webcomponents-react/ComboBox';
 import { ComboBoxItem } from '@ui5/webcomponents-react/ComboBoxItem';
 import { Dialog } from '@ui5/webcomponents-react/Dialog';
 import { Input } from '@ui5/webcomponents-react/Input';
-import { List } from '@ui5/webcomponents-react/List';
-import { ListItemStandard } from '@ui5/webcomponents-react/ListItemStandard';
 import { MultiComboBox } from '@ui5/webcomponents-react/MultiComboBox';
 import { MultiComboBoxItem } from '@ui5/webcomponents-react/MultiComboBoxItem';
 import { MultiInput } from '@ui5/webcomponents-react/MultiInput';
 import { Option } from '@ui5/webcomponents-react/Option';
-import { RadioButton } from '@ui5/webcomponents-react/RadioButton';
 import { Select } from '@ui5/webcomponents-react/Select';
 import { SuggestionItem } from '@ui5/webcomponents-react/SuggestionItem';
-import { Switch } from '@ui5/webcomponents-react/Switch';
 import { Tab } from '@ui5/webcomponents-react/Tab';
 import { TabContainer } from '@ui5/webcomponents-react/TabContainer';
 import { TextArea } from '@ui5/webcomponents-react/TextArea';
-import { Toolbar } from '@ui5/webcomponents-react/Toolbar';
-import { ToolbarButton } from '@ui5/webcomponents-react/ToolbarButton';
 import { useState } from 'react';
 
 export const InputTestComp = () => {
@@ -38,37 +31,6 @@ export const ClearInputTestComp = () => {
       <Input data-testid="test-input" onInput={(e) => setValue(e.target.value)} />
       <span data-testid="input-value">{value}</span>
     </>
-  );
-};
-
-export const CheckboxTestComp = () => {
-  const [checked, setChecked] = useState(false);
-  return (
-    <div>
-      <CheckBox data-testid="test-checkbox" onChange={(e) => setChecked(e.target.checked)} />
-      <span data-testid="checkbox-state">{checked ? 'checked' : 'unchecked'}</span>
-    </div>
-  );
-};
-
-export const SwitchTestComp = () => {
-  const [checked, setChecked] = useState(false);
-  return (
-    <div>
-      <Switch data-testid="test-switch" onChange={(e) => setChecked(e.target.checked)} />
-      <span data-testid="switch-state">{checked ? 'on' : 'off'}</span>
-    </div>
-  );
-};
-
-export const RadioButtonTestComp = () => {
-  const [selected, setSelected] = useState('');
-  return (
-    <div>
-      <RadioButton data-testid="radio-1" name="group" text="Option 1" onChange={() => setSelected('option1')} />
-      <RadioButton data-testid="radio-2" name="group" text="Option 2" onChange={() => setSelected('option2')} />
-      <span data-testid="radio-state">{selected}</span>
-    </div>
   );
 };
 
@@ -96,29 +58,6 @@ export const DialogTestComp = () => {
 
 export const AttributeTestComp = () => {
   return <Button data-testid="test-button">Click me</Button>;
-};
-
-export const ListTestComp = () => {
-  const [selectedItem, setSelectedItem] = useState('');
-  return (
-    <div>
-      <List
-        data-testid="test-list"
-        selectionMode="Single"
-        onSelectionChange={(e) => {
-          const item = e.detail.selectedItems[0];
-          if (item) {
-            setSelectedItem(item.getAttribute('text') || '');
-          }
-        }}
-      >
-        <ListItemStandard text="First Item" />
-        <ListItemStandard text="Second Item" />
-        <ListItemStandard text="Third Item" />
-      </List>
-      <span data-testid="selected-item">{selectedItem}</span>
-    </div>
-  );
 };
 
 export const SelectTestComp = () => {
@@ -169,20 +108,6 @@ export const MultiComboBoxTestComp = () => {
   );
 };
 
-export const ToolbarTestComp = () => {
-  const [clickedButton, setClickedButton] = useState('');
-  return (
-    <div>
-      <Toolbar data-testid="test-toolbar">
-        <ToolbarButton text="Save" onClick={() => setClickedButton('Save')} />
-        <ToolbarButton text="Edit" onClick={() => setClickedButton('Edit')} />
-        <ToolbarButton text="Delete" onClick={() => setClickedButton('Delete')} />
-      </Toolbar>
-      <span data-testid="clicked-button">{clickedButton}</span>
-    </div>
-  );
-};
-
 export const TabContainerTestComp = () => {
   const [selectedTab, setSelectedTab] = useState('Tab 1');
   return (
@@ -213,20 +138,6 @@ export const TabContainerWithNestedTabsTestComp = () => {
         Content 2
       </Tab>
     </TabContainer>
-  );
-};
-
-export const InputWithDelayTestComp = () => {
-  const [value, setValue] = useState('');
-  return (
-    <div>
-      <ComboBox data-testid="test-combobox-delay" onInput={(e) => setValue(e.target.value || '')}>
-        <ComboBoxItem text="Suggestion 1" />
-        <ComboBoxItem text="Suggestion 2" />
-        <ComboBoxItem text="Suggestion 3" />
-      </ComboBox>
-      <span data-testid="delay-input-value">{value}</span>
-    </div>
   );
 };
 
